eda47c06c1e62b15530c66666379c26033fbb8a6
Jeff Cook <jcook@uptycs.com>
2022-09-26 15:45:15 -0700

    Update setup_git.sh

--- setup_git.sh
+++ setup_git.sh
@@ -3,6 +3,7 @@ FIRST_NAME=
 LAST_NAME=
 EMAIL_ADDR=
 
+# https://support.atlassian.com/bitbucket-cloud/docs/configure-your-dvcs-username-for-commits/
 git config --global user.name "$FIRST_NAME $LAST_NAME"
 git config --global user.email "$EMAIL_ADDR"
 
